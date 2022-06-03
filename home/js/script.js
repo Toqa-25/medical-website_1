39858c6df2af01ec5e903ea8c804a686440a45d4
Toqa-25 <toqa35abdelsalam757@gmail.com>
2022-06-03 02:50:55 +0200

    add loadin page

--- home/js/script.js
+++ home/js/script.js
@@ -6,3 +6,10 @@ console.log(nav);
 bar.onclick = function () {
     nav.classList.toggle("nav-bar-show")
 }
+/*************************************************** */
+window.onload = function (){
+    document.querySelector(".loading-section").style.display="none";
+    document.querySelector("body").style.cssText="overflow-y: auto;";
+    console.log("loading")
+
+}
